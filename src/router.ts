d9a49a15babe7b6f480d9ed33ea92fe5c8ee79f7
MarkO <mark_anthony@megabet.com>
2025-12-01 14:57:21 +0800

    init

---
+++ src/router.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import IndexPage from '@/pages/IndexPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: IndexPage,
+    meta: {
+      title: 'Anthony | Portfolio',
+    },
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
